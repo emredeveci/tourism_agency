c863c7c3b9411cec71ec1961234d4b5e67ac4894
Emre Deveci <23077383+emredeveci@users.noreply.github.com>
2024-04-14 13:15:45 +0300

    a missing java class is added to git

---
+++ src/entity/Room.java
@@ -0,0 +1,95 @@
+package entity;
+
+public class Room {
+
+    private int inventory_id;
+    private String hotel_name;
+    private String pension_type;
+    private int discount_id;
+    private String room_type;
+    private int quantity_available;
+    private double adult_price;
+    private double child_price;
+    private int hotel_id;
+    private int room_type_id;
+
+    public int getInventory_id() {
+        return inventory_id;
+    }
+
+    public void setInventory_id(int inventory_id) {
+        this.inventory_id = inventory_id;
+    }
+
+    public int getHotel_id() {
+        return hotel_id;
+    }
+
+    public void setHotel_id(int hotel_id) {
+        this.hotel_id = hotel_id;
+    }
+
+    public int getRoom_type_id() {
+        return room_type_id;
+    }
+
+    public void setRoom_type_id(int room_type_id) {
+        this.room_type_id = room_type_id;
+    }
+
+    public int getQuantity_available() {
+        return quantity_available;
+    }
+
+    public void setQuantity_available(int quantity_available) {
+        this.quantity_available = quantity_available;
+    }
+
+    public String getHotel_name() {
+        return hotel_name;
+    }
+
+    public void setHotel_name(String hotel_name) {
+        this.hotel_name = hotel_name;
+    }
+
+    public String getPension_type() {
+        return pension_type;
+    }
+
+    public void setPension_type(String pension_type) {
+        this.pension_type = pension_type;
+    }
+
+    public int getDiscount_id() {
+        return discount_id;
+    }
+
+    public void setDiscount_id(int discount_id) {
+        this.discount_id = discount_id;
+    }
+
+    public String getRoom_type() {
+        return room_type;
+    }
+
+    public void setRoom_type(String room_type) {
+        this.room_type = room_type;
+    }
+
+    public double getAdult_price() {
+        return adult_price;
+    }
+
+    public void setAdult_price(double adult_price) {
+        this.adult_price = adult_price;
+    }
+
+    public double getChild_price() {
+        return child_price;
+    }
+
+    public void setChild_price(double child_price) {
+        this.child_price = child_price;
+    }
+}
