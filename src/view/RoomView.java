f00abdf9d0897536731a8b262e10b45f58f73d11
Emre Deveci <23077383+emredeveci@users.noreply.github.com>
2024-04-14 19:53:40 +0300

    Finished: room add/update form design

--- src/view/RoomView.java
+++ src/view/RoomView.java
@@ -1,4 +1,25 @@
 package view;
 
+import javax.swing.*;
+
 public class RoomView {
+    private JPanel container;
+    private JComboBox cmb_rooms_hotel_name;
+    private JComboBox cmb_rooms_room_type;
+    private JComboBox cmb_rooms_pension_type;
+    private JComboBox cmb_rooms_season;
+    private JPanel pnl_top;
+    private JTextField fld_rooms_stock;
+    private JTextField fld_rooms_beds;
+    private JTextField fld_rooms_size;
+    private JPanel pnl_mid;
+    private JTextField fld_rooms_adult_price;
+    private JTextField fld_rooms_child_price;
+    private JCheckBox cbox_rooms_television;
+    private JCheckBox cbox_rooms_minibar;
+    private JCheckBox cbox_rooms_console;
+    private JCheckBox cbox_rooms_projector;
+    private JCheckBox cbox_rooms_safe;
+    private JButton SUBMITButton;
+    private JLabel lbl_rooms_title;
 }
